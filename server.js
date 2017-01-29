1950d4043b24710f021410291ba050d1f1a62758
Ramon Recuero <ramon@oly.ai>
2017-01-28 19:23:13 -0500

    Server using es6 as well

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-var express = require('express');
-var path = require('path');
-var webpack = require('webpack');
-var webpackMiddleware = require("webpack-dev-middleware");
-var webpackConfig = require("./webpack.config.js");
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('./webpack.config.js');
 
 // Create our app
-var app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
-    if (req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('http://' + req.hostname + req.url);
-    } else {
-        next();
-    }
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
 });
 
-app.use('/favicon.ico', express.static(__dirname + '[route/to/favicon]'));
+app.use('/favicon.ico',
+  express.static(path.join(__dirname, '[route/to/favicon]')));
 
 app.use(webpackMiddleware(webpack(webpackConfig), {
   quiet: false,
   lazy: false,
   stats: { color: true },
   watchOptions: {
-      aggregateTimeout: 1000,
-      poll: true
+    aggregateTimeout: 1000,
+    poll: true
   },
   publicPath: webpackConfig.output.publicPath
 }));
 
-app.use('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.use(require("webpack-hot-middleware")(webpack(webpackConfig)));
+app.use(require('webpack-hot-middleware')(webpack(webpackConfig)));
 
-app.listen(PORT, function () {
-    console.log('Express server is up on port ' + PORT);
+app.listen(PORT, () => {
+  console.log('Express server is up on port ' + PORT);
 });
